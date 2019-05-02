85fbcda29562b872e7c3836998e393f34249b423
StrugglingDoge <cbckelley@yahoo.com>
2019-05-02 15:46:37 -0400

    Initial commit

---
+++ PaintApp/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PaintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rectColorPicker = new System.Windows.Forms.ColorDialog();
+            this.openSaveDialog = new System.Windows.Forms.OpenFileDialog();
+            this.colorPickerButton = new MaterialSkin.Controls.MaterialLabel();
+            this.savePaintDialog = new System.Windows.Forms.SaveFileDialog();
+            this.saveBitmapDialog = new System.Windows.Forms.SaveFileDialog();
+            this.disableGridLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.setDrawEllipseLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.saveFileLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.openFileLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.resetBoardLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.takePictureLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.drawBoard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.drawBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openSaveDialog
+            // 
+            this.openSaveDialog.Filter = "PaintApp Files (*.pntapp)|*.pntapp";
+            this.openSaveDialog.InitialDirectory = "C:\\";
+            // 
+            // colorPickerButton
+            // 
+            this.colorPickerButton.AutoSize = true;
+            this.colorPickerButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.colorPickerButton.Depth = 0;
+            this.colorPickerButton.Font = new System.Drawing.Font("Roboto", 11F);
+            this.colorPickerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.colorPickerButton.Location = new System.Drawing.Point(659, 75);
+            this.colorPickerButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.colorPickerButton.Name = "colorPickerButton";
+            this.colorPickerButton.Size = new System.Drawing.Size(102, 19);
+            this.colorPickerButton.TabIndex = 1;
+            this.colorPickerButton.Text = "Choose Color";
+            this.colorPickerButton.Click += new System.EventHandler(this.colorPickerButton_Click);
+            // 
+            // savePaintDialog
+            // 
+            this.savePaintDialog.DefaultExt = "pntapp";
+            this.savePaintDialog.Filter = "PaintApp File(*.pntapp)|.pntapp";
+            // 
+            // saveBitmapDialog
+            // 
+            this.saveBitmapDialog.DefaultExt = "png";
+            this.saveBitmapDialog.Filter = "PNG File (*.png)|.png";
+            // 
+            // disableGridLabel
+            // 
+            this.disableGridLabel.AutoSize = true;
+            this.disableGridLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.disableGridLabel.Depth = 0;
+            this.disableGridLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.disableGridLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.disableGridLabel.Location = new System.Drawing.Point(659, 115);
+            this.disableGridLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.disableGridLabel.Name = "disableGridLabel";
+            this.disableGridLabel.Size = new System.Drawing.Size(90, 19);
+            this.disableGridLabel.TabIndex = 2;
+            this.disableGridLabel.Text = "Disable Grid";
+            this.disableGridLabel.Click += new System.EventHandler(this.DisableGridLabel_Click);
+            // 
+            // setDrawEllipseLabel
+            // 
+            this.setDrawEllipseLabel.AutoSize = true;
+            this.setDrawEllipseLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.setDrawEllipseLabel.Depth = 0;
+            this.setDrawEllipseLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.setDrawEllipseLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.setDrawEllipseLabel.Location = new System.Drawing.Point(659, 155);
+            this.setDrawEllipseLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.setDrawEllipseLabel.Name = "setDrawEllipseLabel";
+            this.setDrawEllipseLabel.Size = new System.Drawing.Size(62, 19);
+            this.setDrawEllipseLabel.TabIndex = 3;
+            this.setDrawEllipseLabel.Text = "Ellipses";
+            this.setDrawEllipseLabel.Click += new System.EventHandler(this.setDrawEllipseLabel_Click);
+            // 
+            // saveFileLabel
+            // 
+            this.saveFileLabel.AutoSize = true;
+            this.saveFileLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveFileLabel.Depth = 0;
+            this.saveFileLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.saveFileLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.saveFileLabel.Location = new System.Drawing.Point(659, 355);
+            this.saveFileLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.saveFileLabel.Name = "saveFileLabel";
+            this.saveFileLabel.Size = new System.Drawing.Size(69, 19);
+            this.saveFileLabel.TabIndex = 4;
+            this.saveFileLabel.Text = "Save File";
+            this.saveFileLabel.Click += new System.EventHandler(this.saveFileLabel_Click);
+            // 
+            // openFileLabel
+            // 
+            this.openFileLabel.AutoSize = true;
+            this.openFileLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.openFileLabel.Depth = 0;
+            this.openFileLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.openFileLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.openFileLabel.Location = new System.Drawing.Point(659, 395);
+            this.openFileLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.openFileLabel.Name = "openFileLabel";
+            this.openFileLabel.Size = new System.Drawing.Size(71, 19);
+            this.openFileLabel.TabIndex = 5;
+            this.openFileLabel.Text = "Open File";
+            this.openFileLabel.Click += new System.EventHandler(this.openFileLabel_Click);
+            // 
+            // resetBoardLabel
+            // 
+            this.resetBoardLabel.AutoSize = true;
+            this.resetBoardLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.resetBoardLabel.Depth = 0;
+            this.resetBoardLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.resetBoardLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.resetBoardLabel.Location = new System.Drawing.Point(659, 235);
+            this.resetBoardLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.resetBoardLabel.Name = "resetBoardLabel";
+            this.resetBoardLabel.Size = new System.Drawing.Size(47, 19);
+            this.resetBoardLabel.TabIndex = 6;
+            this.resetBoardLabel.Text = "Reset";
+            this.resetBoardLabel.Click += new System.EventHandler(this.resetBoardLabel_Click);
+            // 
+            // takePictureLabel
+            // 
+            this.takePictureLabel.AutoSize = true;
+            this.takePictureLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.takePictureLabel.Depth = 0;
+            this.takePictureLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.takePictureLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.takePictureLabel.Location = new System.Drawing.Point(659, 275);
+            this.takePictureLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.takePictureLabel.Name = "takePictureLabel";
+            this.takePictureLabel.Size = new System.Drawing.Size(85, 19);
+            this.takePictureLabel.TabIndex = 7;
+            this.takePictureLabel.Text = "Screenshot";
+            this.takePictureLabel.Click += new System.EventHandler(this.takePictureLabel_Click);
+            // 
+            // drawBoard
+            // 
+            this.drawBoard.BackColor = System.Drawing.Color.Transparent;
+            this.drawBoard.Location = new System.Drawing.Point(12, 75);
+            this.drawBoard.Name = "drawBoard";
+            this.drawBoard.Size = new System.Drawing.Size(641, 641);
+            this.drawBoard.TabIndex = 0;
+            this.drawBoard.TabStop = false;
+            this.drawBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawBoard_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 729);
+            this.Controls.Add(this.takePictureLabel);
+            this.Controls.Add(this.resetBoardLabel);
+            this.Controls.Add(this.openFileLabel);
+            this.Controls.Add(this.saveFileLabel);
+            this.Controls.Add(this.setDrawEllipseLabel);
+            this.Controls.Add(this.disableGridLabel);
+            this.Controls.Add(this.colorPickerButton);
+            this.Controls.Add(this.drawBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Sizable = false;
+            this.Text = "Paint";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.drawBoard)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawBoard;
+        private System.Windows.Forms.ColorDialog rectColorPicker;
+        private System.Windows.Forms.OpenFileDialog openSaveDialog;
+        private MaterialSkin.Controls.MaterialLabel colorPickerButton;
+        private System.Windows.Forms.SaveFileDialog savePaintDialog;
+        private System.Windows.Forms.SaveFileDialog saveBitmapDialog;
+        private MaterialSkin.Controls.MaterialLabel disableGridLabel;
+        private MaterialSkin.Controls.MaterialLabel setDrawEllipseLabel;
+        private MaterialSkin.Controls.MaterialLabel saveFileLabel;
+        private MaterialSkin.Controls.MaterialLabel openFileLabel;
+        private MaterialSkin.Controls.MaterialLabel resetBoardLabel;
+        private MaterialSkin.Controls.MaterialLabel takePictureLabel;
+    }
+}
+
---
+++ PaintApp/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace PaintApp
+{
+    public partial class Form1 : MaterialForm
+    {
+
+        /*
+         * 
+         * 
+         * Having separate colors for rectangles in the array may not be possible
+         * -- The issue is caused because in the paint function, we are constantly
+         * looping through the array index drawing all the currently existing rectangles.
+         * Without that, the rectangles obviously would not draw. So, in order to fix
+         * I would need to switch to a different way of drawing the rectangles -- which
+         * I have no idea how to do :/.
+         * 
+         *
+         */
+
+        Rectangle[] rect;
+        int _x = 0;
+        int _y = 0;
+        int length;
+        static Brush testbrush = Brushes.Blue;
+        Color color = ((SolidBrush)testbrush).Color;
+        bool showGrid = true;
+        bool drawEllipse = false;
+        bool takingScreenshot = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //set up material form
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey900, Primary.Grey900, Primary.Grey900, 0, TextShade.WHITE);
+            //done setting up material form
+
+            //set up rectangle array
+            rect = new Rectangle[1];
+            rect[0] = new Rectangle(_x, _y, 32, 32);
+            //done
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.W:
+                    if (rect[0].Y != 0)
+                        rect[0].Y -= 32;
+                    break;
+                case Keys.Down:
+                case Keys.S:
+                    if (rect[0].Y != 608)
+                        rect[0].Y += 32;
+                    break;
+                case Keys.Left:
+                case Keys.A:
+                    if (rect[0].X != 0)
+                        rect[0].X -= 32;
+                    break;
+                case Keys.Right:
+                case Keys.D:
+                    if (rect[0].X != 608)
+                        rect[0].X += 32;
+                    break;
+                case Keys.M:
+                case Keys.E:
+                    deleteBlock();
+                    break;
+                case Keys.Space:
+                    placeBlock();
+                    break;
+            }
+            drawBoard.Invalidate();
+        }
+
+        private void DrawBoard_Paint(object sender, PaintEventArgs e)
+        {
+            // so damn ugly, but it works /shrug
+
+            if (!drawEllipse)
+            {
+                for (int i = rect.Length - 1; i > 0; i--)
+                    e.Graphics.FillRectangle(testbrush, rect[i]);
+
+                if(!takingScreenshot)
+                    e.Graphics.FillRectangle(Brushes.Red, rect[0]); // player
+                if (showGrid)
+                {
+                    for (int x = 0; x < 20; x++) //simple 20x20 grid.
+                        for (int y = 0; y < 20; y++)
+                            e.Graphics.DrawRectangle(Pens.Black, x * 32, y * 32, 32, 32);
+                }
+            }
+            else
+            {
+                for (int i = rect.Length - 1; i > 0; i--)
+                    e.Graphics.FillEllipse(testbrush, rect[i]);
+
+                if (!takingScreenshot)
+                    e.Graphics.FillEllipse(Brushes.Red, rect[0]); // player
+                if (showGrid)
+                {
+                    for (int x = 0; x < 20; x++) //simple 20x20 grid.
+                        for (int y = 0; y < 20; y++)
+                            e.Graphics.DrawEllipse(Pens.Black, x * 32, y * 32, 32, 32);
+                }
+            }
+            
+        }
+
+        private void placeBlock()
+        {
+            for (int i = rect.Length - 1; i > 0; i--)
+                if (rect[0].Contains(rect[i])) //make sure the player isn't placing a rectangle where one already exists.
+                    return;
+            Rectangle testrec;
+            List<Rectangle> placeblock = rect.ToList();
+            placeblock.Add(testrec = new Rectangle(rect[rect.Length - 1].X, rect[rect.Length - 1].Y, 32, 32)); // create new rectangle, set x,y to player pos and add to array index.
+            rect = placeblock.ToArray();
+            for (int i = rect.Length - 1; i > 0; i--)
+                rect[i] = rect[i - 1]; //assign correct array index for new rectangle.
+            drawBoard.Invalidate();
+        }
+
+        private void resetBoard() // reset paint board
+        {
+            if (rect.Length == 1) // if just player is left, we return, no point in clearing an already cleared board
+                return;
+            List<Rectangle> removeall = rect.ToList();
+            for (int i = rect.Length - 1; i > 0; i--) // loop through array index and remove all, except index 0 which is player
+            {
+                removeall.Remove(rect[i]);
+                rect = removeall.ToArray();
+            }
+            drawBoard.Invalidate();
+        }
+
+        private void deleteBlock()
+        {//reverse of what placeBlock() does.
+            List<Rectangle> removeblock = rect.ToList();
+            for (int i = rect.Length - 1; i > 0; i--)
+            {
+                if (rect[0].X == rect[i].X && rect[0].Y == rect[i].Y) // if player x, y = another rectangles position, find that rectangle in the array index and remove it.
+                {
+                    removeblock.RemoveAt(i);
+                    rect = removeblock.ToArray();
+                }
+            }
+        }
+
+        private void screenshotBoard()
+        {
+            //Maybe implement a resizing feature?
+            if (saveBitmapDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(drawBoard.Width, drawBoard.Height); // create a new blank bitmap
+
+                drawBoard.DrawToBitmap(bmp, new Rectangle(0, 0,
+                            drawBoard.Width, drawBoard.Height)); // now, draw graphics from drawBoard onto the blank bitmap
+
+                bmp.Save(saveBitmapDialog.FileName, ImageFormat.Png); // save the bitmap as a png file and done :)
+            }
+        }
+
+        #region Files
+
+        private void savePaintFile(string location)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(location);
+                for (int i = rect.Length - 1; i > 0; i--)
+                {
+                    //write x coordinate, newline, write y coordinate, next rectangle, etc.
+                    sw.WriteLine(rect[i].X);
+                    sw.WriteLine(rect[i].Y);
+                }
+
+                sw.WriteLine(drawEllipse);
+                sw.WriteLine(color.ToArgb());
+
+                sw.Close();
+
+                MessageBox.Show("Successfully saved to: " + location);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nError writing to file: " + location);
+            }
+        }
+
+        private void loadSaveFile(string location)
+        {
+            //first find length of the selected file, then readline
+
+            length = 0;
+            try
+            {
+                StreamReader sr1 = new StreamReader(location);
+
+                string test;
+
+                while ((test = sr1.ReadLine()) != null)
+                {
+                    length++;
+                }
+
+                if(length > 801) // 800 is filled board, simple check to make sure they're not trying to add more blocks then whats allowed
+                {
+                    MessageBox.Show("Are you sure this is a PaintApp file? Seems to be to large to be, stopping...");
+                    sr1.Close();
+                    return;
+                }
+
+                sr1.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nwat, somehow we were able to find the file but not get length?");
+                return;
+            }
+
+            try
+            {
+
+                StreamReader sr = new StreamReader(location);
+
+                resetBoard(); // reset board, no overlapping artwork
+
+                //this first step we actually place/create rectangles
+                for (int i = 0; i < length / 2 - 1; i++) // we divide by two because 1 block takes two lines in file.
+                {
+                    // super simple, for each line in text file, we read next line, set rectx = to x in file and recty = y in file and place.
+                    try
+                    {
+                        rect[0].X = Convert.ToInt32(sr.ReadLine());
+                        rect[0].Y = Convert.ToInt32(sr.ReadLine());
+                        placeBlock();
+                    }catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                //second step is to find out if we should enable ellipses and find out what color to set the rectangles to.
+                try
+                {
+                    drawEllipse = Convert.ToBoolean(sr.ReadLine());
+                    color = new SolidBrush(Color.FromArgb(Convert.ToInt32(sr.ReadLine()))).Color;
+                    testbrush = new SolidBrush(color);
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                //reset player pos so theyre not in the middle of nowhere
+                rect[0].X = 0;
+                rect[0].Y = 0;
+
+                sr.Close();//close text file.
+                MessageBox.Show("Successfully opened: " + location);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nOkay seriously. How did you manage to do this? We were able to successfully find the length of the file but not able to place rects in correct pos.");
+            }
+        }
+
+        #endregion
+
+        private void DisableGridLabel_Click(object sender, EventArgs e)
+        {
+            if (showGrid)
+            {
+                showGrid = false;
+                disableGridLabel.Text = "Enable Grid";
+                drawBoard.Invalidate();
+            }
+            else
+            {
+                showGrid = true;
+                disableGridLabel.Text = "Disable Grid";
+                drawBoard.Invalidate();
+            }
+        }
+
+        private void colorPickerButton_Click(object sender, EventArgs e)
+        {
+            if (rectColorPicker.ShowDialog() == DialogResult.OK)
+            {
+                color = new SolidBrush(rectColorPicker.Color).Color;
+
+                testbrush = new SolidBrush(color);
+            }
+            drawBoard.Invalidate();
+        }
+
+        private void setDrawEllipseLabel_Click(object sender, EventArgs e)
+        {
+            if (drawEllipse)
+            {
+                drawEllipse = false;
+                setDrawEllipseLabel.Text = "Ellipses";
+                drawBoard.Invalidate();
+            }
+            else
+            {
+                drawEllipse = true;
+                setDrawEllipseLabel.Text = "Rectangles";
+                drawBoard.Invalidate();
+            }
+        }
+
+        private void resetBoardLabel_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("This will clear the entire board!\nAre you sure you want to do this?",
+                        "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if (dr == DialogResult.Yes)
+            {
+                resetBoard();
+            }
+        }
+
+        private void saveFileLabel_Click(object sender, EventArgs e)
+        {
+            if (savePaintDialog.ShowDialog() == DialogResult.OK)
+                savePaintFile(savePaintDialog.FileName);
+        }
+
+        private void openFileLabel_Click(object sender, EventArgs e)
+        {
+            if (openSaveDialog.ShowDialog() == DialogResult.OK)
+                loadSaveFile(openSaveDialog.FileName);
+        }
+
+        private void takePictureLabel_Click(object sender, EventArgs e)
+        {
+            takingScreenshot = true; //setting this bool to true hides the player(red controllable block)
+            drawBoard.Invalidate();
+            screenshotBoard();
+            takingScreenshot = false;
+            drawBoard.Invalidate();
+        }
+    }
+}
